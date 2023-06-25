0218e3c7854c1d395c8f5302062f700762f0c293
BrozekEmeilia87 <137542757+BrozekEmeilia87@users.noreply.github.com>
2023-06-25 13:40:30 +0700

    Refactor SQL queries

---
+++ queen_elephant_house.js
@@ -0,0 +1,86 @@
+//DIY Home and Beauty Recipe Blog
+
+//Index Page
+
+//CSS
+
+<style>
+	body {
+		font-family: Arial;
+		text-align: center;
+	}
+	h1 {
+		font-size: 24px;
+		font-weight: bold;
+		color: #4169E1
+	}
+	h2 {
+		font-size: 16px;
+		font-weight: normal;
+		margin-bottom: 8px;
+	}
+	.blog-post {
+		margin-top: 20px;
+		border: 1px solid #ddd;
+		width: 100%;
+		padding: 25px;
+		background-color: #f2f2f2;
+		text-align: left;
+	}
+</style>
+
+//HTML
+
+<html>
+<head>
+	<title>My DIY and Beauty Recipe Blog</title>
+</head>
+<body>
+	<h1>My DIY and Beauty Recipes</h1>
+	<h2>Welcome to my blog! Here you'll find some of my favorite DIY and beauty recipes.</h2>
+	<div class="blog-post">
+		<h3>How to Make an All Natural Facial Mask</h3>
+		<p>
+			This all natural facial mask is perfect for getting rid of impurities and leaving your skin feeling refreshed and glowing! Here's what you'll need:
+		</p>
+		<ul>
+			<li>1/2 cup of oatmeal</li>
+			<li>1/4 cup of honey</li>
+			<li>1 egg white</li>
+			<li>1/4 teaspoon of lemon juice</li>
+		</ul>
+		<p>
+			Combine all ingredients in a bowl and mix until they are fully blended. Apply to clean face and leave on for 15 minutes. Rinse off with warm water and pat dry with a clean cloth.
+		</p>
+	</div>
+	<div class="blog-post">
+		<h3>DIY Hair Treatment Using Coconut Oil</h3>
+		<p>
+			This is an amazing DIY hair treatment that will leave your hair feeling soft and manageable. Here's what you'll need:
+		</p>
+		<ul>
+			<li>1/4 cup of coconut oil</li>
+			<li>1/4 cup of olive oil</li>
+			<li>1 tablespoon of honey</li>
+		</ul>
+		<p>
+			Mix all ingredients together in a bowl. Apply to clean, damp hair and massage into scalp. Wrap hair in a warm towel and leave on for 30 minutes. Rinse with warm water and style as usual.
+		</p>
+	</div>
+	<div class="blog-post">
+		<h3>How to Make a Natural Toothpaste</h3>
+		<p>
+			Making your own toothpaste is a great way to avoid harsh chemicals and maintain healthy, white teeth. Here's what you'll need:
+		</p>
+		<ul>
+			<li>2 tablespoons of baking soda</li>
+			<li>1 tablespoon of coconut oil</li>
+			<li>10 drops of peppermint essential oils</li>
+		</ul>
+		<p>
+			Mix all ingredients together in a small bowl. Transfer to an airtight container and store in a cool, dry place. Use as you would normal toothpaste.
+		</p>
+	</div>
+
+</body>
+</html>
